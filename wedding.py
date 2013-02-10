c835601fbb4c86878d450f4f5a25512c1e97807f
Daniel Bruckner <dmeirb@gmail.com>
2013-02-10 06:47:44 +0400

    supporting splash, added debug, pointed static at media

--- wedding.py
+++ wedding.py
@@ -1,11 +1,19 @@
 from flask import Flask
+from flask import render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='media')
+
+app.debug = True
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def splash():
+    return render_template("splash.html") 
+
+@app.route("/welcome")
+def welcome():
+    return render_template("index.html")
+
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0")
 
