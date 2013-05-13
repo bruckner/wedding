beccf5d0e11857a292bfe1f0633d70e654fa4e93
Daniel Bruckner <dmeirb@gmail.com>
2013-05-13 11:29:10 +0400

    now with logistics info!

--- wedding.py
+++ wedding.py
@@ -11,11 +11,49 @@ def splash():
 
 @app.route("/welcome")
 def welcome():
-    return render_template("welcome.html")
+    default = '/work-in-progress'
+    menuitems = [
+        {'title': 'OUR STORY', 'target': '/romance'},
+        {'title': 'OUR PROPOSAL', 'target': '/proposal'},
+        {'title': 'SHEDULE of EVENTS', 'target': '/schedule'},
+        {'title': 'ACCOMODATIONS', 'target': '/hotel'},
+        {'title': 'TRAVEL', 'target': '/travel'},
+        {'title': 'REGISTRY', 'target': 
+            '/work-in-progress'},
+    ]
+    return render_template("welcome.html", menuitems=menuitems)
+
+@app.route("/details/<what>")
+def details(what):
+    return render_template("%s-details.html" % what)
+
+@app.route("/schedule")
+def events():
+    return render_template("events.html")
+
+@app.route("/travel")
+def travel():
+    return render_template("travel.html")
+
+@app.route("/hotel")
+def hotel():
+    return render_template("hotel.html")
+
+@app.route("/romance")
+def romance():
+    return render_template("romance.html")
+
+@app.route("/proposal")
+def proposal():
+    return render_template("proposal.html")
 
 @app.route("/work-in-progress")
 def wip():
     return render_template("construction.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
